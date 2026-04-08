1fd9fac1f0e66431f6d7e4f994d8036a7d992f35
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-08 12:59:40 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,6 +4,14 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader= " "
 
+-- telescope settings
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
