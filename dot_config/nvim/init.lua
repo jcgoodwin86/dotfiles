091c8823cbd32da66197524e43d0ad3b32388828
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-08 13:27:45 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,62 +2,55 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-
   vim.fn.system({
-
     "git",
-
     "clone",
-
     "--filter=blob:none",
-
     "https://github.com/folke/lazy.nvim.git",
-
-    "--branch=stable", -- latest stable release
-
+    "--branch=stable",
     lazypath,
-
   })
-
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-
-local opts = {}
-
-local plugins = {
-
+require("lazy").setup({
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
   {
-    'nvim-telescope/telescope.nvim', version = '0.2.2',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-  }
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+  },
+})
 
-}
-
-require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
---set the colorscheme to it!
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 
-local builtin = require('telescope.builtin')
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<C-p>', function()
-  builtin.find_files({
-    hidden = true,
-    no_ignore = true,
-  })
-end)
+telescope.setup({
+  defaults = {
+    file_ignore_patterns = {},
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+      no_ignore = true,
+    },
+  },
+})
 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+pcall(telescope.load_extension, "fzf")
 
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
