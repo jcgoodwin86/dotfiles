8339bcf5708cb6df7180e88207923c2e38133ae3
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-08 13:00:43 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,14 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader= " "
 
--- telescope settings
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -57,4 +49,10 @@ require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
 --set the colorscheme to it!
 vim.cmd.colorscheme "catppuccin"
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
