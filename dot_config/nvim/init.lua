fdafa724598e68215c73cb697963c681aa10a3ba
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-08 12:50:12 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -35,8 +35,11 @@ vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 
-local plugins = {}
+local plugins = {
 
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+
+}
 
 
 require("lazy").setup(plugins, opts)
