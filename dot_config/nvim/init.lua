029345e5e56a32e2100d221907dccab18bc8021a
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-10 11:38:48 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -53,6 +53,8 @@ require("lazy").setup({
     end,
   },
 
+  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"}
+
 })
 
 -- =============================================================================
