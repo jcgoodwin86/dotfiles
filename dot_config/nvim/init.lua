70b4d6afd5f8e9100b9ed1b0bacb05a4d61c4adc
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-10 12:17:10 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -55,17 +55,27 @@ require("lazy").setup({
 
   -- Syntax highlighting and indentation
   {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "javascript", "go", "yaml", "json", "bash" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  config = function()
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.templ = {
+      install_info = {
+        url = "https://github.com/vrischmann/tree-sitter-templ",
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "master",
+      },
+      filetype = "templ",
+    }
+
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "javascript", "go", "yaml", "json", "bash", "templ" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+},
 })
 
 -- =============================================================================
