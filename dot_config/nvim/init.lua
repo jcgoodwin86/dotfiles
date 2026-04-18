5399e4685e1e80e40e1ba22aa24c848e615c0d4b
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2026-04-17 23:30:03 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- Entry point for Neovim config.
 -- Loads options first (before plugins), then bootstraps the plugin manager,
--- then keymaps last (so plugins are available when keymaps are defined).
+-- then keymaps last (so plugins are available when keymaps are defined)
 
 require("config.options")
 require("config.lazy")
